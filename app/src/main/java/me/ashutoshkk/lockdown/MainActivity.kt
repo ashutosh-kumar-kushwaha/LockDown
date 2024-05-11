79b97c3b430ff064493d13998af9dcf47ce816be
Ashutosh <akkushwaha0104@gmail.com>
2024-05-11 21:36:06 +0530

    Add AppLockService

--- app/src/main/java/me/ashutoshkk/lockdown/MainActivity.kt
+++ app/src/main/java/me/ashutoshkk/lockdown/MainActivity.kt
@@ -1,16 +1,22 @@
 package me.ashutoshkk.lockdown
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import me.ashutoshkk.lockdown.service.AppLockService
 import me.ashutoshkk.lockdown.ui.theme.LockDownTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +24,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val context = LocalContext.current
             LockDownTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding).clickable {
+                            val intent = Intent(context, AppLockService::class.java)
+                            startService(intent)
+                            Toast.makeText(context, "Start", Toast.LENGTH_SHORT).show()
+                        }
                     )
                 }
             }
